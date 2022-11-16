e120e8e285ef9dd982d2709b8066e5f16e318a7d
Jet-Green <grishadzyin@gmail.com>
2022-11-16 16:46:32 +0400

    add simpliest camera in the world

--- src/views/Camera.vue
+++ src/views/Camera.vue
@@ -1,53 +1,33 @@
 <script setup>
-import { ref, onMounted } from 'vue'
-import { WebCam } from 'vue-camera-lib'
-
-let webcam = null
-let cameras = []
-let deviceId = ref('')
-const emit = defineEmits(['init', 'photoTaken'])
-
-async function takePhoto() {
-    try {
-        await webcam.takePhoto();
-    } catch (err) {
-        console.log(err)
-    }
-}
-function loadCameras() {
-    webcam.loadCameras()
-    cameras = webcam.cameras;
-}
-function webcamInit(deviceId) {
-    deviceId.value = deviceId
-    emit('init', deviceId)
-}
-function setCamera() {
-    webcam.changeCamera(deviceId.value === '' ? null : deviceId.value)
-}
-
-function photoTakenEvent({ blob, image_data_url }) {
-    emit('photoTaken', { blob, image_data_url })
-}
-
+import { onMounted } from 'vue'
 onMounted(() => {
-    cameras = webcam.cameras;
-    if (cameras.length === 0) {
-        // if no camera found, we will try to refresh cameras list each second until there is some camera
-        loadCameras()
-        // let reloadCamInterval = setInterval(() => {
-        //     if (cameras.length > 0) {
-        //         clearInterval(reloadCamInterval)
-        //     }
-        // }, 1000);
+
+    // Grab elements, create settings, etc.
+    var video = document.getElementById('video');
+
+    // Get access to the camera!
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        // Not adding `{ audio: true }` since we only want video now
+        navigator.mediaDevices.getUserMedia({ video: true }).then(function (stream) {
+            //video.src = window.URL.createObjectURL(stream);
+            video.srcObject = stream;
+            video.play();
+        });
     }
+
+    // Elements for taking the snapshot
+    var canvas = document.getElementById('canvas');
+    var context = canvas.getContext('2d');
+    var video = document.getElementById('video');
+
+    // Trigger photo take
+    document.getElementById("snap").addEventListener("click", function () {
+        context.drawImage(video, 0, 0, 640, 480);
+    });
 })
 </script>
 <template>
-    <WebCam ref="webcam" @photoTaken="photoTakenEvent" @init="webcamInit" />
-    <select @change="setCamera" v-model="deviceId">
-        <option value="">-</option>
-        <option v-for="camera in cameras" :value="camera.deviceId">{{ camera }}</option>
-    </select>
-    <button @click="takePhoto">Take a photo</button>
+    <video id="video" width="640" height="480" autoplay></video>
+    <button id="snap">Snap Photo</button>
+    <canvas id="canvas" width="640" height="480"></canvas>
 </template>
