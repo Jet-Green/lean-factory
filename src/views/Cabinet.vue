2c6f115517bf99b908c8d273d1143d052e3bd211
Jet-Green <grishadzyin@gmail.com>
2022-12-11 19:06:21 +0400

    add services and start cabinet

--- src/views/Cabinet.vue
+++ src/views/Cabinet.vue
@@ -1,4 +1,25 @@
-<script setup></script>
+<script setup>
+import { useAuth } from "../stores/auth"
+const userStore = useAuth()
+
+const reportsToFix = userStore.user.reportsToFix
+const reports = userStore.user.reports
+
+</script>
 <template>
-  <h1>Cabinet</h1>
+  <v-row>
+    <v-col cols="12">
+      <h2>Нужно исправить</h2>
+      <span v-if="!reportsToFix.length">
+        Пусто
+      </span>
+    </v-col>
+
+    <v-col cols=" 12">
+      <h2>Отправил на исправление</h2>
+      <span v-if="!reports.length">
+        Пусто
+      </span>
+    </v-col>
+  </v-row>
 </template>
