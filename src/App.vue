c68000e8db6a16bd53292e1ca816c1f2040da27b
JetGreen <grishadzyin@gmail.com>
2023-02-14 14:10:39 +0400

    rm top bar

--- src/App.vue
+++ src/App.vue
@@ -31,13 +31,13 @@ onMounted(async () => {
 
 <template>
     <v-layout>
-        <v-app-bar color="primary" class="d-none d-md-flex">
-            <!-- <v-app-bar-title>Photos</v-app-bar-title> -->
+        <!-- <v-app-bar color="primary" class="d-none d-md-flex"> -->
+        <!-- <v-app-bar-title>Photos</v-app-bar-title> -->
 
-            <!-- <template v-slot:prepend>
+        <!-- <template v-slot:prepend>
                 <v-btn icon="mdi-dots-vertical"></v-btn>
             </template> -->
-        </v-app-bar>
+        <!-- </v-app-bar> -->
         <v-navigation-drawer expand-on-hover rail class="d-none d-md-flex">
             <v-list density="compact" nav @click:select="move">
                 <v-list-item color="red" prepend-icon="mdi-camera" title="Нарушение" value="Problem"
